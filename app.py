e497bbc637fe2cdf2ee779532c27adddd849fc50
fsfesdsfs <68592763+fsfesdsfs@users.noreply.github.com>
2026-05-07 12:41:02 +0300

    Switch from PaddleOCR to EasyOCR
    Replaced PaddleOCR with EasyOCR for image text recognition.

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-pip install paddleocr
-from paddleocr import PaddleOCR
-
-ocr = PaddleOCR(lang='en')
-
-result = ocr.ocr("image.png")
-
-for line in result[0]:
-    print(line[1][0])
+import easyocr
 
 st.title("Разпознаване на вредни съставки в храни")
 
